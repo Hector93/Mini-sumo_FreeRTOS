fd9f62f53b6d7ebdb270a91c04f528ae3e826ed1
Hector93 <hmanuelg93@gmail.com>
2020-07-08 20:51:09 -0500

    se integran los id de procesos
    se integran por que cada que tengo que generar un nuevo proceso tengo
    que abrir todos las definiciones de procesos para poder ver que id es
    el que sigue en la secuencia

---
+++ Ids.h
@@ -0,0 +1,19 @@
+#ifndef __IDS_H
+#define __IDS_H
+
+
+#define serialID  1
+#define motorRID 2
+#define motorLID 3
+#define sensorsDistID 4
+#define sensorsFloorID 5
+#define imuId 6
+#define miniId 7
+#define oledID  8
+#define irID 9
+
+//type irprotocol
+#define COMMAND 1
+//address of the remote controller
+#define ADDRESSID 16
+#endif
